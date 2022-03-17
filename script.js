3613cd97455200175bd1d3ffdb4d096acf97e9db
Diego <pureduck101@gmail.com>
2022-03-16 18:19:20 -0700

    Add draw with hover

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const container = document.querySelector('#container');
 const INITIAL_GRID = 16;
-
-for (let r = 0; r < INITIAL_GRID; r++) {
-    const row = document.createElement('div');
-    container.appendChild(row);
-    row.setAttribute('id',`${r+1}`);
-    for (let c = 0; c < INITIAL_GRID; c++) {
-        const div = document.createElement('div');
-        div.setAttribute('class', 'colorTile');
-        row.appendChild(div);
+function makeTiles(rowNum, columnNum) {
+    for (let r = 0; r < rowNum; r++) {
+        const row = document.createElement('div');
+        container.appendChild(row);
+        row.setAttribute('id', `${r + 1}`);
+        for (let c = 0; c < columnNum; c++) {
+            const div = document.createElement('div');
+            div.setAttribute('class', 'colorTile');
+            row.appendChild(div);
+        }
     }
-}   
-
+}
+function setupTiles(){
+    makeTiles(INITIAL_GRID, INITIAL_GRID);
+    const allTiles = document.querySelectorAll('.colorTile');
+    allTiles.forEach( (tile)=>{
+        tile.addEventListener('mouseover',drawOnTile);
+    })
+}
+function drawOnTile(e){
+    e.target.style.background= "black";
+}
+setupTiles();
 
 
 
