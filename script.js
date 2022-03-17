87fded66d382be4670634667fa924b9a2f53f99d
Diego <pureduck101@gmail.com>
2022-03-16 19:04:47 -0700

    Allow clear to set new grid

--- script.js
+++ script.js
@@ -8,28 +8,43 @@ function makeTiles(rowNum, columnNum) {
     for (let r = 0; r < rowNum; r++) {
         const row = document.createElement('div');
         container.appendChild(row);
-        row.setAttribute('id', `${r + 1}`);
+        row.setAttribute('class', `row`);
         for (let c = 0; c < columnNum; c++) {
             const div = document.createElement('div');
             div.setAttribute('class', 'colorTile');
             row.appendChild(div);
         }
     }
+
 }
-function setupTiles(){
-    makeTiles(INITIAL_GRID, INITIAL_GRID);
+function setupTiles(row, col) {
+    makeTiles(row, col);
     const allTiles = document.querySelectorAll('.colorTile');
-    allTiles.forEach( (tile)=>{
-        tile.addEventListener('mouseover',drawOnTile);
+    allTiles.forEach((tile) => {
+        tile.addEventListener('mouseover', drawOnTile);
     })
+
 }
-function drawOnTile(e){
-    e.target.style.background= "black";
+
+function drawOnTile(e) {
+    e.target.style.background = "black";
 }
-function clearTiles(){
-    const allTiles = document.querySelectorAll('.colorTile');
-    allTiles.forEach( (tile)=>{
-        tile.style.background = "white";
-    })
+function removeTiles(){
+    const rows = document.querySelectorAll('.row');
+    const childTiles = document.querySelectorAll('.colorTile');
+    if(!rows) return;
+    for(const row of rows){
+        container.removeChild(row);
+    }
+
+
+    
 }
-setupTiles();
+
+function clearTiles() {
+    removeTiles();
+    const rowAndCol = parseInt(prompt("Enter grid desired"));
+    setupTiles(rowAndCol, rowAndCol);
+}
+
+setupTiles(INITIAL_GRID, INITIAL_GRID);
